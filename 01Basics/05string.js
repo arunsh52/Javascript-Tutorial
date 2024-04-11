c161641cbb06d07afd9f068ac0a97505f06a2a16
Arun sharma <84015635+arunsh52@users.noreply.github.com>
2024-04-11 13:12:51 +0000

    include string file

---
+++ 01Basics/05string.js
@@ -0,0 +1,17 @@
+const name = "Javascript"
+
+// console.log(name + " is best language");
+
+console.log(`${name} is a best language in the world!`);
+
+const anotherName = new String("React") // This is another way to create string but this will create a object
+
+console.log(`${anotherName} is a framework of ${name}`);
+
+
+console.log(name.toUpperCase());
+console.log(name.charAt(9));
+console.log(name.indexOf('t'));
+console.log(name.concat(anotherName))
+console.log(name.slice(3));
+console.log(name.substring(2,5))
