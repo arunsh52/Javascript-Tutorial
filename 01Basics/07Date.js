02fd6a836d4ab3a0bb73d865665fc8d616fa7c8f
Arun sharma <84015635+arunsh52@users.noreply.github.com>
2024-04-12 13:23:31 +0000

    add date file

---
+++ 01Basics/07Date.js
@@ -0,0 +1,11 @@
+const myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+
+let myCreateDate = new Date(2024,3,12,6,25);
+console.log(myCreateDate.toString())
